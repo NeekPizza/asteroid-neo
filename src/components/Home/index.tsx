eaec58f9e54bec60e181b3be9c12b851f5432546
Nick Piazza <nick@onlinemeded.org>
2021-08-17 03:23:48 -0500

    implement accordion for data

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,13 +1,20 @@
 import { useState } from "react";
 import axios from "axios";
 import format from "utils/formatDate";
+import Accordion from "@material-ui/core/Accordion";
+import AccordionSummary from "@material-ui/core/AccordionSummary";
 import Button from "@material-ui/core/Button";
-import Grid from "@material-ui/core/Grid";
-import DatePicker from "components/UI/DatePicker";
 import CircularProgress from "@material-ui/core/CircularProgress";
+import DatePicker from "components/UI/DatePicker";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles({
+  accordionContainer: {
+    width: "100%",
+    marginTop: "20px",
+  },
   marginTopGrid: {
     marginTop: "20px",
   },
@@ -89,6 +96,23 @@ const Home = () => {
           <CircularProgress />
         </Grid>
       )}
+      <Grid container>
+        {neos && (
+          <div className={classes.accordionContainer}>
+            {Object.keys(neos).map((key: string, index) => (
+              <Accordion>
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls='panel1a-content'
+                  id='panel1a-header'
+                >
+                  <p>{key}</p>
+                </AccordionSummary>
+              </Accordion>
+            ))}
+          </div>
+        )}
+      </Grid>
     </Grid>
   );
 };
